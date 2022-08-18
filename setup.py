75ce9e373e0ec0df3701da50d0234a764e8b8042
Gael Pasgrimaud <gael@gawel.org>
2022-08-18 10:55:56 +0200

    Back to development: 0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.8'
+version = '0.9.dev0'
 
 install_requires = ['six', 'waitress', 'webob']
 test_requires = [
